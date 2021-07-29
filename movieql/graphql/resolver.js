fd389531532db2865a4b73312da1cce62d60a659
kwb020312 <kwb020312@naver.com>
2021-07-29 09:27:33 +0900

    Using YTS API

--- movieql/graphql/resolver.js
+++ movieql/graphql/resolver.js
@@ -1,12 +1,7 @@
-import { getById, getMovies, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 export const resolvers = {
   Query: {
-    movie: (_, { id }) => getById(id),
     movies: () => getMovies(),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
