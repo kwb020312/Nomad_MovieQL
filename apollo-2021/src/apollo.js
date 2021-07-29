838d77e371acd1cf66b169694010d04a6ebd0408
kwb020312 <kwb020312@naver.com>
2021-07-29 11:24:07 +0900

    Client Connect To Server!

--- apollo-2021/src/apollo.js
+++ apollo-2021/src/apollo.js
@@ -1,5 +1,5 @@
-import { ApolloClient } from "apollo-boost";
+import ApolloClient from "apollo-boost";
 
 export const client = new ApolloClient({
-  uri: "https://movieql.now.sh/",
+  uri: "http://localhost:4000/",
 });
--- apollo-2021/src/routes/Home.js
+++ apollo-2021/src/routes/Home.js
@@ -1,5 +1,21 @@
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 import React from "react";
 
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
 export const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (error) return `Error! : ${error}`;
+  if (loading) return "Loading...";
+  if (data && data.movies) return data.movies.map((m) => <h1>{m.id}</h1>);
+  console.log(data);
   return <div>Home</div>;
 };
