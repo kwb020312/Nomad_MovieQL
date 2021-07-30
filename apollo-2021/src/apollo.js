f13a3cf77cf885cdc78312ad69e17eb8ddee1b4e
kwb020312 <kwb020312@naver.com>
2021-07-30 10:31:38 +0900

    Styled Components & Save Cache

--- apollo-2021/src/apollo.js
+++ apollo-2021/src/apollo.js
@@ -2,4 +2,19 @@ import ApolloClient from "apollo-boost";
 
 export const client = new ApolloClient({
   uri: "http://localhost:4000/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: !isLiked,
+          },
+        });
+      },
+    },
+  },
 });
